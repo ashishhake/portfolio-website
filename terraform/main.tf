7edcb0506a355b9a22936e23fb6b947c47ad4f47
Ashish <ashishhake007@gmail.com>
2026-03-10 19:51:06 +0530

    updated terraform infrastructure

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,99 +1,99 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 6.0"
-        }
+  required_version = ">= 1.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
     }
+  }
 }
 
 provider "aws" {
-    region = var.default_aws_region
+  region = var.default_aws_region
 }
 
 resource "aws_s3_bucket" "static_web_bucket" {
-    bucket = var.bucket_name
+  bucket = var.bucket_name
 
-    tags = {
-        Name = "s3_bucket_for_portfolio_website"
-    }
+  tags = {
+    Name = "s3_bucket_for_portfolio_website"
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "portfolio_website" {
-    bucket = aws_s3_bucket.static_web_bucket.id
+  bucket = aws_s3_bucket.static_web_bucket.id
 
-    block_public_acls       = true
-    block_public_policy     = true
-    ignore_public_acls      = true
-    restrict_public_buckets = true
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_versioning" "portfolio_website" {
-    bucket = aws_s3_bucket.static_web_bucket.id
-    versioning_configuration {
-        status = "Enabled"
-    }
+  bucket = aws_s3_bucket.static_web_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
-resource "aws_s3_bucket_website_configuration" "portfolio_website" {
-    bucket = aws_s3_bucket.static_web_bucket.id
-
-    index_document {
-        suffix = "index.html"
-    }
-
-    error_document {
-        key = "404.html"
-    }
-}
 
 resource "aws_cloudfront_origin_access_control" "portfolio_website" {
-    name                              = "static-website-oac"
-    description                       = "Allow CloudFront to access S3"
-    origin_access_control_origin_type = "s3"
-    signing_behavior                  = "always"
-    signing_protocol                  = "sigv4"
+  name                              = "static-website-oac"
+  description                       = "Allow CloudFront to access S3"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
 }
 
 resource "aws_cloudfront_distribution" "portfolio_website" {
-    enabled             = true
-    default_root_object = "index.html"
+  enabled             = true
+  default_root_object = "index.html"
 
-    origin {
-        domain_name = aws_s3_bucket.static_web_bucket.bucket_regional_domain_name
-        origin_id = "s3-origin"
+  origin {
+    domain_name              = aws_s3_bucket.static_web_bucket.bucket_regional_domain_name
+    origin_id                = "s3-origin"
+    origin_access_control_id = aws_cloudfront_origin_access_control.portfolio_website.id
+    s3_origin_config {
+      origin_access_identity = ""
     }
+  }
 
-    default_cache_behavior {
-        viewer_protocol_policy = "redirect-to-https"
-        target_origin_id = "s3-origin"
-        allowed_methods  = ["GET", "HEAD"]
-        cached_methods   = ["GET", "HEAD"]
+  default_cache_behavior {
+    viewer_protocol_policy = "redirect-to-https"
+    target_origin_id       = "s3-origin"
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
 
-        forwarded_values {
-            query_string = false
+    forwarded_values {
+      query_string = false
 
-            cookies {
-                forward = "none"
-            }
-        }
+      cookies {
+        forward = "none"
+      }
     }
+  }
 
-    restrictions {
-        geo_restriction {
-        restriction_type = "none"
-        }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
     }
+  }
 
-    viewer_certificate {
-        cloudfront_default_certificate = true
-    }
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
 
-    price_class = "PriceClass_200"
+  price_class = "PriceClass_200"
 }
 
 resource "aws_s3_bucket_policy" "static_site" {
+
   bucket = aws_s3_bucket.static_web_bucket.id
+
+  depends_on = [
+    aws_cloudfront_distribution.portfolio_website
+  ]
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -105,6 +105,12 @@ resource "aws_s3_bucket_policy" "static_site" {
         }
         Action   = "s3:GetObject"
         Resource = "${aws_s3_bucket.static_web_bucket.arn}/*"
+
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.portfolio_website.arn
+          }
+        }
       }
     ]
   })
